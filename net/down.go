1a0bfd2fc0f373700ad98f98c629bcf5177cef92
Nathan Osman <nathan@quickmediasolutions.com>
2017-01-13 13:05:55 -0800

    Add command to check if a website is down.

---
+++ net/down.go
@@ -0,0 +1,38 @@
+package net
+
+import (
+	"fmt"
+	"net/http"
+	"regexp"
+
+	"github.com/nathan-osman/george-the-dev-bot/registry"
+	"github.com/nathan-osman/go-sechat"
+)
+
+var isDownRegexp = regexp.MustCompile(
+	`(?i)\bis\s+([\w.-]+)\s+down\b`,
+)
+
+func init() {
+	registry.Register(func(c *sechat.Conn, e *sechat.Event) {
+		m := isDownRegexp.FindStringSubmatch(e.TextContent)
+		if m != nil {
+			go func() {
+				r, err := http.Get(
+					fmt.Sprintf("http://%s", m[1]),
+				)
+				if err != nil || r.StatusCode >= 400 {
+					c.Reply(e, fmt.Sprintf(
+						"%s seems to be down",
+						m[1],
+					))
+					return
+				}
+				c.Reply(e, fmt.Sprintf(
+					"%s works for me",
+					m[1],
+				))
+			}()
+		}
+	})
+}
