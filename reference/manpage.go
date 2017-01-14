de387b9396e395ee1b0b5a11ded5f5b81011d5a9
Nathan Osman <nathan@quickmediasolutions.com>
2017-01-13 20:07:49 -0800

    Simplified manpage.

--- reference/manpage.go
+++ reference/manpage.go
@@ -2,7 +2,6 @@ package reference
 
 import (
 	"fmt"
-	"net/http"
 	"regexp"
 
 	"github.com/nathan-osman/george-the-dev-bot/registry"
@@ -17,25 +16,12 @@ func init() {
 	registry.Register(func(c *sechat.Conn, e *sechat.Event) {
 		m := manpageRegexp.FindStringSubmatch(e.TextContent)
 		if m != nil {
-			go func() {
-				// Grab the subexpressions that matched
-				item := m[1] + m[2] + m[3]
-				r, err := http.Get(
-					"http://manpages.ubuntu.com/cgi-bin/search.py?titles=404&q=" + item,
-				)
-				if err != nil || r.StatusCode >= 400 {
-					c.Reply(e, fmt.Sprintf(
-						"unable to find manpage for %s.",
-						item,
-					))
-					return
-				}
-				c.Reply(e, fmt.Sprintf(
-					"[manpage for %s](%s)",
-					item,
-					r.Request.URL.String(),
-				))
-			}()
+			item := m[1] + m[2] + m[3]
+			c.Reply(e, fmt.Sprintf(
+				"[manpage for %s](%s)",
+				item,
+				"http://manpages.ubuntu.com/cgi-bin/search.py?titles=404&q="+item,
+			))
 		}
 	})
 }
