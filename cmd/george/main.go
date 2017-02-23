ca2fb091f1743ee3245455444b91f3aa659e321b
Nathan Osman <nathan@quickmediasolutions.com>
2017-02-23 12:09:27 -0800

    Switch to urfave/cli.

--- cmd/george/main.go
+++ cmd/george/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,6 +9,7 @@ import (
 	"github.com/nathan-osman/george-the-dev-bot/server"
 	"github.com/nathan-osman/go-sechat"
 	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
 
 	_ "github.com/nathan-osman/george-the-dev-bot/apt"
 	_ "github.com/nathan-osman/george-the-dev-bot/net"
@@ -20,46 +20,81 @@ import (
 var log = logrus.New()
 
 func main() {
-	var (
-		addr     = flag.String("addr", ":8000", "HTTP address")
-		debug    = flag.Bool("debug", false, "enable debugging")
-		email    = flag.String("email", os.Getenv("EMAIL"), "email address")
-		password = flag.String("password", os.Getenv("PASSWORD"), "login password")
-		room     = flag.Int("room", 1, "room to join")
-	)
-	flag.Parse()
-	if *debug {
-		logrus.SetLevel(logrus.DebugLevel)
+	app := cli.NewApp()
+	app.Name = "george"
+	app.Usage = "run George the Dev bot"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "addr",
+			Value:  ":8000",
+			EnvVar: "ADDR",
+			Usage:  "HTTP address",
+		},
+		cli.BoolFlag{
+			Name:   "debug",
+			EnvVar: "DEBUG",
+			Usage:  "enable debugging",
+		},
+		cli.StringFlag{
+			Name:   "email",
+			EnvVar: "EMAIL",
+			Usage:  "email address",
+		},
+		cli.StringFlag{
+			Name:   "password",
+			EnvVar: "PASSWORD",
+			Usage:  "login password",
+		},
+		cli.IntFlag{
+			Name:   "room",
+			Value:  1,
+			EnvVar: "ROOM",
+			Usage:  "initial room to join",
+		},
 	}
-	if *email == "" || *password == "" {
-		log.Fatal("email and password cannot be blank")
-	}
-	s, err := server.New(*addr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer s.Close()
-	log.Print("initializing chat connection")
-	c, err := sechat.New(*email, *password, *room)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer c.Close()
-	defer log.Print("closing chat connection")
-	ch := make(chan os.Signal)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	for {
-		select {
-		case <-ch:
-			return
-		case e := <-c.Events:
-			switch {
-			// If the user is invited, have him join the room
-			case e.EventType == sechat.EventInvitation:
-				c.Join(e.RoomID)
-			case e.IsMention:
-				registry.Execute(c, e)
+	app.Action = func(c *cli.Context) error {
+		if c.Bool("debug") {
+			logrus.SetLevel(logrus.DebugLevel)
+		}
+		var (
+			email    = c.String("email")
+			password = c.String("password")
+		)
+		if email == "" || password == "" {
+			log.Fatal("email and password cannot be blank")
+		}
+		srv, err := server.New(c.String("addr"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer srv.Close()
+		log.Print("initializing chat connection")
+		conn, err := sechat.New(
+			c.String("email"),
+			c.String("password"),
+			c.Int("room"),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer conn.Close()
+		defer log.Print("closing chat connection")
+		ch := make(chan os.Signal)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		for {
+			select {
+			case <-ch:
+				return nil
+			case e := <-conn.Events:
+				switch {
+				// If the user is invited, have him join the room
+				case e.EventType == sechat.EventInvitation:
+					conn.Join(e.RoomID)
+				case e.IsMention:
+					registry.Execute(conn, e)
+				}
 			}
 		}
 	}
+	app.Run(os.Args)
 }
